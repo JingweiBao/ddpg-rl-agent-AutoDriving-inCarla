40732db3560c527a16b0676bbdc1c5143916048b
JingweiBao <72962168+JingweiBao@users.noreply.github.com>
2024-05-11 00:25:34 -0500

    Add files via upload

---
+++ rl/__init__.py
@@ -0,0 +1,5 @@
+import rl.augmentations
+
+from rl.agents import DDPGAgent
+from rl.agents.ddpg import DDPGMemory
+from rl.environments.carla import *
---
+++ rl/networks/__init__.py
@@ -0,0 +1,3 @@
+
+from rl.networks.networks import Network, DDPGNetwork
+from rl.networks.architectures import shufflenet_v2
